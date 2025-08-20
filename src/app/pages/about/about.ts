9b2a9e49f8db8a44f64a7cb8ddcaf58ac23c0c9b
Harindra Masna <86703116+harindra-dev@users.noreply.github.com>
2025-08-21 03:25:06 +0530

    fix: firebase ci cd issues

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.html',
-  styleUrl: './about.scss'
+  styleUrls: ['./about.scss'],
 })
 export class About {
-
+  // about page logic
 }
